cc6dfb756a4443aab94772c3700b71f286545999
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-11-25 17:18:03 +0200

    hw_4-updated

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -4,23 +4,20 @@ const { Conflict } = require("http-errors");
 
 const register = async (req, res, next) => {
 
-    const { name, email, password, subscription } = req.body;
+    const {email, password, subscription } = req.body;
     const user = await User.findOne({ email });
     if (user) {
         throw new Conflict(409, "Email in use");
     }
 
     const result = await User.create({
-        name,
         email,
         password,
         subscription,
     });
     res.status(201).json({
-        user: {
-            email: result.email,
-            subscription: result.subscription,
-        },
+        email: result.email,
+        subscription: result.subscription,
     });
 };
 
