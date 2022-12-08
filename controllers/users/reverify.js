d4f455994b3e1ffac61fce3a9b5bbf62bcd80bc9
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-12-08 14:46:43 +0200

    updated

--- controllers/users/reverify.js
+++ controllers/users/reverify.js
@@ -9,11 +9,6 @@ const reverify = async(req, res) => {
   const user = await User.findOne({ email });
   const { verificationToken } = user;
 
-  if (!email) {
-    throw new BadRequest("Missing required field email");
-  };
-  
-
   if (user.verify) {
     throw new BadRequest("Verification has already been passed");
   };
